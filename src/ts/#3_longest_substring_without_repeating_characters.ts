b7b44bd8405574e8ee4b8f42782b1f02131e4a15
jewook <jewook3617@gmail.com>
2021-03-23 17:00:29 +0900

    add: #3, #4

---
+++ src/ts/#3_longest_substring_without_repeating_characters.ts
@@ -0,0 +1,23 @@
+/**
+ * 문제 : https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ */
+
+type IndexMap = {
+  [key: string]: number;
+};
+
+function lengthOfLongestSubstring(s: string): number {
+  const indexMap: IndexMap = {};
+
+  let leftIndex: number = 0;
+
+  return s
+    .split("")
+    .reduce((answer: number, char: string, rightIndex: number) => {
+      leftIndex = indexMap[char] >= leftIndex ? indexMap[char] + 1 : leftIndex;
+      indexMap[char] = rightIndex;
+      return Math.max(answer, rightIndex - leftIndex + 1);
+    }, 0);
+}
+
+console.log(lengthOfLongestSubstring("pwwkew"));
