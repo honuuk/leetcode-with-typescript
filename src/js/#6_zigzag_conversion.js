6244fd8e811fed26ae73b3364cbd6328dc2e7592
jewook <jewook3617@gmail.com>
2021-04-08 09:54:34 +0900

    add: #6

---
+++ src/js/#6_zigzag_conversion.js
@@ -0,0 +1,19 @@
+"use strict";
+/**
+ * 문제 : https://leetcode.com/problems/zigzag-conversion/
+ */
+function convert(s, numRows) {
+    const arr = Array(Math.min(numRows, s.length)).fill("");
+    let currentRow = 0;
+    let isGoingDown = true;
+    for (const char of s) {
+        if (numRows === 1 || numRows > s.length)
+            return s;
+        arr[currentRow] = arr[currentRow].concat(char);
+        currentRow = isGoingDown ? currentRow + 1 : currentRow - 1;
+        if (currentRow === 0 || currentRow === numRows - 1)
+            isGoingDown = !isGoingDown;
+    }
+    return arr.reduce((acc, str) => acc + str, "");
+}
+console.log(convert("PAYPALISHIRING", 3));
---
+++ src/ts/#6_zigzag_conversion.ts
@@ -0,0 +1,23 @@
+/**
+ * 문제 : https://leetcode.com/problems/zigzag-conversion/
+ */
+
+function convert(s: string, numRows: number): string {
+  const arr: string[] = Array(Math.min(numRows, s.length)).fill("");
+
+  let currentRow = 0;
+  let isGoingDown = true;
+
+  for (const char of s) {
+    if (numRows === 1 || numRows > s.length) return s;
+
+    arr[currentRow] = arr[currentRow].concat(char);
+    currentRow = isGoingDown ? currentRow + 1 : currentRow - 1;
+    if (currentRow === 0 || currentRow === numRows - 1)
+      isGoingDown = !isGoingDown;
+  }
+
+  return arr.reduce((acc, str) => acc + str, "");
+}
+
+console.log(convert("PAYPALISHIRING", 3));
