12a4a5a3e9ac9089993393660c3251d01b6fdefe
jewook <jewook3617@gmail.com>
2021-05-26 10:35:14 +0900

    add: #11

---
+++ src/#11-container-with-most-water.ts
@@ -0,0 +1,20 @@
+/**
+ * 문제 : https://leetcode.com/problems/container-with-most-water/
+ */
+
+function maxArea(height: number[]): number {
+  let answer = 0,
+    l = 0,
+    r = height.length - 1;
+
+  while (l < r) {
+    answer = Math.max(answer, (r - l) * Math.min(height[l], height[r]));
+
+    if (height[l] < height[r]) l++;
+    else r--;
+  }
+
+  return answer;
+}
+
+// console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
