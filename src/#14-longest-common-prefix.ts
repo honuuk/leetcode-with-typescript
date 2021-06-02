37699852215aba6ffedd33889486abd282263c98
jewook <jewook3617@gmail.com>
2021-06-02 11:13:27 +0900

    fix: #14 함수 분리

--- src/#14-longest-common-prefix.ts
+++ src/#14-longest-common-prefix.ts
@@ -2,16 +2,24 @@
  * 문제 : https://leetcode.com/problems/longest-common-prefix/
  */
 
+function LCP(left: string, right: string) {
+  let prefix = left;
+
+  while (right.indexOf(prefix) !== 0) {
+    prefix = prefix.slice(0, prefix.length - 1);
+    if (prefix === "") break;
+  }
+
+  return prefix;
+}
+
 function longestCommonPrefix(strs: string[]): string {
   if (strs.length === 0) return "";
 
   let answer = strs[0];
 
   for (const str of strs) {
-    while (str.indexOf(answer) !== 0) {
-      answer = answer.slice(0, answer.length - 1);
-    }
-
+    answer = LCP(answer, str);
     if (answer === "") break;
   }
 
