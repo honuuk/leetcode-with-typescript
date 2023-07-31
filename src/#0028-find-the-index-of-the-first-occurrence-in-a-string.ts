93d0a88d1ab9f2834f75c99f1c8e440eb914003f
jewook3617 <jewook3617@gmail.com>
2023-07-31 18:59:16 +0900

    add #0028

---
+++ src/#0028-find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -0,0 +1,19 @@
+/**
+ * 문제 : https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+ * 난이도 : Easy
+ */
+
+export function strStr(haystack: string, needle: string): number {
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] !== needle[0]) continue;
+
+    for (let j = 0; j < needle.length; j++) {
+      if (haystack[i + j] !== needle[j]) break;
+      if (j === needle.length - 1) return i;
+    }
+  }
+
+  return -1;
+}
+
+console.log(strStr("sadbutsad", "sad"));
